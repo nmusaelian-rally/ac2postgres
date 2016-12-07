765249f5199584562f3ed8d21403c71ca246fd61
nmusaelian-rally <nmusaelian@rallydev.com>
2016-12-07 16:57:52 -0700

    dele and create db, get table names

---
+++ utils/get_data.py
@@ -0,0 +1,49 @@
+import psycopg2
+from psycopg2.extensions import AsIs
+import yaml
+import pytest
+
+'''
+to check outcomes in terminal:
+    $ sudo su postgres
+    bash-3.2$ psql -l
+'''
+
+with open('../config.yaml', 'r') as file:
+    config = yaml.load(file)
+
+DB   = config["db"]["name"]
+USER = config["db"]["user"]
+PASS = config["db"]["password"]
+HOST = config["db"]["host"]
+PORT = config["db"]["port"]
+
+
+def get_tables(dbname=DB):
+    con = None # to avoid "local variable con might be referenced before assignement
+    tables = []
+    try:
+        con = psycopg2.connect(database=dbname, user=USER, password=PASS, host=HOST, port=PORT)
+        cur = con.cursor()
+        cur.execute("""SELECT table_name FROM information_schema.tables
+           WHERE table_schema = 'public'""")
+
+        cur.execute("""SELECT table_name FROM information_schema.tables
+               WHERE table_schema = 'public'""")
+        for table in cur.fetchall():
+            tables.append(table)
+
+    except psycopg2.Error as e:
+        print("oh, noes! " + e.pgerror)
+    finally:
+        if con:
+            con.close()
+            return tables
+
+
+def test_get_tables():
+    tables = get_tables()
+    assert len(tables)
+    print("\n-----\n")
+    for t in tables:
+        print(t[0]) # printing first element of each tuple, e.g. defect of (defect,)
