f375924f9873465f2d5de77e3fe1626cfb97a5d7
nmusaelian-rally <nmusaelian@rallydev.com>
2016-12-30 15:50:15 -0700

    created historic data US12,US13

--- playground/dbcon.py
+++ playground/dbcon.py
@@ -14,7 +14,8 @@ class DBCon:
         self.entities   = self.config['db']['tables'].replace(',','').split()
         self.schema     = self.get_schema()
         self.index      = 0
-        self.columns = {}
+        self.columns    = {}
+        self.cache_columns()
 
     def read_config(self, config_name):
         with open(config_name, 'r') as file:
@@ -56,12 +57,17 @@ class DBCon:
         found = element['ElementName'] in self.config["db"]["columns"]
         return found
 
+    def cache_columns(self):
+        for itemtype in self.schema:
+            attributes = list(filter(self.attributes_subset, itemtype['Attributes']))
+            table_name = itemtype['Name']
+            self.columns[table_name] = [{attr['ElementName']: attr['AttributeType']} for attr in attributes ]
+
 
     def create_tables_n_columns(self):
         for itemtype in self.schema:
             attributes = list(filter(self.attributes_subset, itemtype['Attributes']))
             table_name = itemtype['Name']
-            self.columns[table_name] = [{attr['ElementName']: attr['AttributeType']} for attr in attributes ]
             self.cursor.execute("CREATE TABLE %s ();", (AsIs(table_name),))
             self.cursor.execute("ALTER TABLE %s ADD COLUMN ID SERIAL PRIMARY KEY;",(AsIs(table_name),))
             self.cursor.execute("ALTER TABLE %s ADD COLUMN _start TIME WITH TIME ZONE;", (AsIs(table_name),))
@@ -108,6 +114,58 @@ class DBCon:
                 expression = "VALUES (%s)" % formatters % tuple(field_values)
                 self.cursor.execute("INSERT INTO %s (%s) %s", (AsIs(entity), AsIs(columns), AsIs(expression),))
             self.cursor.execute("UPDATE %s SET _start = %s", (AsIs(entity), datetime.now(timezone.utc),))
-        self.index += 1
+        self.db.commit()
+        self.db.close()
+
+    def update(self):
+        self.index += 3
+        for entity in self.entities:
+            records_to_update = []
+            second_cursor = self.db.cursor()
+            fields = [k for column in self.columns[entity] for k,v in column.items()]
+            fetch = ','.join(fields)
+            file_name = "%sz_%s.json" %(entity,self.index)
+            response = self.read_data(file_name)
+            for item in response:
+                field_values = []
+                formatters = ""
+                empty_fields = []
+                for field in fields:
+                    value = item[field]
+                    # RATING   type e.g. Severity     when empty return 'None'.
+                    # QUANTITY type e.g. PlanEstimate when empty return None
+                    if not value or value == 'None':
+                        empty_fields.append(field)
+                    else:
+                        formatters = formatters + "%s,"
+                        number_fields = [k for column in self.columns[entity] for k,v in column.items() if 'INTEGER' in column.values() or 'QUANTITY' in column.values()]
+                        if field not in number_fields and field != 'None':
+                            value = "'" + value + "'"
+                        field_values.append(value)
+                non_empty_fields = fields[:]
+                for field in empty_fields:
+                    non_empty_fields.remove(field)
+                columns = ','.join(non_empty_fields)
+                formatters = formatters[:-1]  # remove trailing comma
+                expression = "VALUES (%s)" % formatters % tuple(field_values)
+                for field in non_empty_fields:
+                    # there will be multiple rows with the same name if there are multiple snapshots of the same records
+                    # get current snapshots (_end IS NULL) for each items
+                    # fetch id and each field one execution of SELECT command at a time for each record
+                    self.cursor.execute("SELECT id, %s FROM %s WHERE _end IS NULL and name = '%s';",
+                                        (AsIs(field), AsIs(entity), AsIs(item['Name']),))
+                    # compare the value of the same field in the item from response with corresponding value in the db:
+                    for row in self.cursor:
+                        #print("comparing %s with %s" %(item[field], row[1]))
+                        if item[field] != row[1]:
+                            #print ("DIFF! record id %s" %row[0])
+                            records_to_update.append(row[0])
+                            second_cursor.execute("INSERT INTO %s (%s) %s RETURNING id;",(AsIs(entity), AsIs(columns), AsIs(expression),))
+                            new_row_id = second_cursor.fetchone()[0]
+                            print ("LAST inserted ID: %s" %new_row_id)
+                            second_cursor.execute("UPDATE %s SET _start = %s WHERE id = %s",
+                                     (AsIs(entity), datetime.now(timezone.utc), AsIs(new_row_id),))
+            for id in records_to_update:
+                self.cursor.execute("UPDATE %s SET _end = %s WHERE id = %s", (AsIs(entity), datetime.now(timezone.utc), AsIs(id),))
         self.db.commit()
         self.db.close()
