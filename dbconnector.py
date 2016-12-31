4f0e89fedd1f7996833960df0c80a9d434146362
nmusaelian-rally <nmusaelian@rallydev.com>
2016-12-31 15:14:16 -0700

    added update func to  dbconnector

--- dbconnector.py
+++ dbconnector.py
@@ -4,6 +4,7 @@ import psycopg2
 from psycopg2.extensions import AsIs
 import yaml
 from pyral import Rally, rallyWorkset, RallyRESTAPIError
+from datetime import datetime, timezone
 
 class DBConnector:
     def __init__(self, config):
@@ -14,6 +15,7 @@ class DBConnector:
         self.entities   = self.config['db']['tables'].replace(',','').split()
         self.schema     = self.get_schema()
         self.columns = {}
+        self.cache_columns()
 
     def read_config(self, config_name):
         with open(config_name, 'r') as file:
@@ -84,6 +86,11 @@ class DBConnector:
         found = element.ElementName in self.config["ac"]["fetch"]
         return found
 
+    def cache_columns(self):
+        for itemtype in self.schema:
+            attributes = list(filter(self.attributes_subset, itemtype.Attributes))
+            table_name = itemtype.ElementName
+            self.columns[table_name] = [{attr.ElementName: attr.AttributeType} for attr in attributes ]
 
     def create_tables_n_columns(self):
         for itemtype in self.schema:
@@ -91,9 +98,11 @@ class DBConnector:
             table_name = itemtype.ElementName
             #populate list of dictionaries of column_name:type, e.g.
             # [{'CreationDate': 'DATE'}, {'ObjectID': 'INTEGER'}, {'ScheduleState': 'STATE'}]
-            self.columns[table_name] = [{attr.ElementName: attr.AttributeType} for attr in attributes ]
+            #self.columns[table_name] = [{attr.ElementName: attr.AttributeType} for attr in attributes ]
             self.cursor.execute("CREATE TABLE %s ();", (AsIs(table_name),))
             self.cursor.execute("ALTER TABLE %s ADD COLUMN ID SERIAL PRIMARY KEY;",(AsIs(table_name),))
+            self.cursor.execute("ALTER TABLE %s ADD COLUMN _start TIME WITH TIME ZONE;", (AsIs(table_name),))
+            self.cursor.execute("ALTER TABLE %s ADD COLUMN _end TIME WITH TIME ZONE;", (AsIs(table_name),))
             for attr in attributes:
                 element_name = attr.ElementName
                 attribute_type = attr.AttributeType
@@ -102,10 +111,7 @@ class DBConnector:
                 print('-' + element_name)
                 print('---' + attribute_type)
 
-                if attr.ElementName == 'ObjectID':
-                    self.cursor.execute("ALTER TABLE %s ADD COLUMN %s bigint",
-                                (AsIs(table_name), AsIs(element_name),))
-                elif attr.AttributeType == 'RATING':
+                if attr.AttributeType == 'RATING':
                     rating_allowed_values = [a.StringValue for a in allowed_values]
                     self.cursor.execute("ALTER TABLE %s ADD COLUMN %s text check (%s IN (%s)) ",
                                 (AsIs(table_name), AsIs(element_name), AsIs(element_name),
@@ -118,6 +124,8 @@ class DBConnector:
                 else:
                     self.cursor.execute("ALTER TABLE %s ADD COLUMN %s %s",
                                 (AsIs(table_name), AsIs(element_name), (AsIs(self.matchTypes(attribute_type))),))
+
+
             self.db.commit()
 
     def insert_init_data(self):
@@ -149,5 +157,59 @@ class DBConnector:
                 formatters = formatters[:-1] #remove trailing comma
                 expression = "VALUES (%s)" % formatters % tuple(field_values)
                 self.cursor.execute("INSERT INTO %s (%s) %s", (AsIs(entity), AsIs(columns), AsIs(expression),))
+            self.cursor.execute("UPDATE %s SET _start = %s", (AsIs(entity), datetime.now(timezone.utc),))
         self.db.commit()
         self.db.close()
+
+    def update(self):
+        query = self.config['ac']['query']
+        for entity in self.entities:
+            records_to_update = []
+            second_cursor = self.db.cursor()
+            fields = [k for column in self.columns[entity] for k, v in column.items()]
+            fetch = ','.join(fields)
+            response = self.ac.get('%s' % entity, fetch=fetch, query=query, order="ObjectID", pagesize=200)
+            for item in response:
+                field_values = []
+                formatters = ""
+                empty_fields = []
+                for field in fields:
+                    value = getattr(item, field)
+                    # RATING   type e.g. Severity     when empty return 'None'.
+                    # QUANTITY type e.g. PlanEstimate when empty return None
+                    if not value or value == 'None':
+                        empty_fields.append(field)
+                    else:
+                        formatters = formatters + "%s,"
+                        number_fields = [k for column in self.columns[entity] for k,v in column.items() if 'INTEGER' in column.values() or 'QUANTITY' in column.values()]
+                        if field not in number_fields and field != 'None':
+                            value = "'" + value + "'"
+                        field_values.append(value)
+                non_empty_fields = fields[:]
+                for field in empty_fields:
+                    non_empty_fields.remove(field)
+                columns = ','.join(non_empty_fields)
+                formatters = formatters[:-1]  # remove trailing comma
+                expression = "VALUES (%s)" % formatters % tuple(field_values)
+                for field in non_empty_fields:
+                    # there will be multiple rows with the same name if there are multiple snapshots of the same records
+                    # get current snapshots (_end IS NULL) for each items
+                    # fetch id and each field one execution of SELECT command at a time for each record
+                    self.cursor.execute("SELECT id, %s FROM %s WHERE _end IS NULL and ObjectID = '%s';",
+                                        (AsIs(field), AsIs(entity), AsIs(item.ObjectID),))
+                    # compare the value of the same field in the item from response with corresponding value in the db:
+                    for row in self.cursor:
+                        #print("comparing %s with %s" %(item[field], row[1]))
+                        if getattr(item, field) != row[1]:
+                            #print ("DIFF! record id %s" %row[0])
+                            records_to_update.append(row[0])
+                            second_cursor.execute("INSERT INTO %s (%s) %s RETURNING id;",(AsIs(entity), AsIs(columns), AsIs(expression),))
+                            new_row_id = second_cursor.fetchone()[0]
+                            print ("LAST inserted ID: %s" %new_row_id)
+                            second_cursor.execute("UPDATE %s SET _start = %s WHERE id = %s",
+                                     (AsIs(entity), datetime.now(timezone.utc), AsIs(new_row_id),))
+            for id in records_to_update:
+                self.cursor.execute("UPDATE %s SET _end = %s WHERE id = %s", (AsIs(entity), datetime.now(timezone.utc), AsIs(id),))
+        self.db.commit()
+        self.db.close()
+
